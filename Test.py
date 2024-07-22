99238b86ba292e13deaa5c009c666e0451a7f34c
Soumyojjwal Sett <settsoumyojjwal9876@gmail.com>
2024-07-23 02:53:06 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,7 @@
+# Well Well Well You are doing good Studying CS50 Python Class
+
+name = input("Your Name Mister ? ")
+print("Hello Mr ", name)
+print("Dont You have Anything More to do ?"),
+Ans = input("Yes/No")
+print("ok", Ans)
