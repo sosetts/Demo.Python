e33f867e49bf174144ee521572ff04e6d6df8852
Soumyojjwal Sett <settsoumyojjwal9876@gmail.com>
2024-07-27 15:26:36 +0530

    ist Commit

--- Test.py
+++ Test.py
@@ -1,7 +1,25 @@
 # Well Well Well You are doing good Studying CS50 Python Class
+# Addition of 2 Numbers
 
-name = input("Your Name Mister ? ")
-print("Hello Mr ", name)
-print("Dont You have Anything More to do ?"),
-Ans = input("Yes/No")
-print("ok", Ans)
+Number1 = 12
+Number2 = 45
+print(Number1 + Number2)
+
+x = 10
+y = "20"
+print(str(x) + y)
+
+x = 10
+y = 20
+x, y = y, x
+print(x, y)
+
+print(complex(2, 3))
+print(-7//2)
+print(7//2)
+print(7/2)
+
+result = 10 + 5 * 2 ** 3 // 4 - 1
+print(result)
+print(type(result))
+
