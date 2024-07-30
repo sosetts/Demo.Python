3221afa22bbfa90afc9a8c08f8ac59f5591860dc
Soumyojjwal Sett <settsoumyojjwal9876@gmail.com>
2024-07-31 01:02:42 +0530

    just Learning Dont Judge,

--- Test.py
+++ Test.py
@@ -1,38 +1,31 @@
 # Well Well Well You are doing good Studying CS50 Python Class
 # Addition of 2 Numbers
-
+Number1 = 69
 Number2 = 45
 print(Number1 + Number2)
-
 x = 10
 y = "20"
 print(str(x) + y)
-
 x = 10
 y = 20
 x, y = y, x
 print(x, y)
-
 print(complex(2, 3))
 print(-7//2)
 print(7//2)
 print(7/2)
-
 result = 10 + 5 * 2 ** 3 // 4 - 1
 print(result)
 print(type(result))
-
 type(Number1)
-
  #Inside to Outside First Float Then String Then Output
 a = 98
 print (str(float(a))) 
-
 xx = 67%7
 print(xx)
-
 # Converting "5" String to 5 integer
 a ="5"
 b = int(a)
 print(b)
-
+well = do = you = know = about = it = 989
+print (well+do+you+know+about+it)
